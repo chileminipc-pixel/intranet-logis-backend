6a1c8459a67771151ef6506a1fd082ac1aaa4783
Alvaro <chileminipc@gmail.com>
2025-10-07 20:28:15 -0300

    fix: Agrego zona horaria a la conexoin parra que fuerce a vercel + railway a entregar info correcta

--- config/bd.js
+++ config/bd.js
@@ -13,7 +13,9 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  timezone: '-04:00'
+
 });
 console.log('✅ Conexión a MariaDB establecida');
 
